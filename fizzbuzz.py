753db79b90921143da34e10968115b2fddc0a88a
mariellabanez <mariellabanez0814@gmail.com>
2025-10-31 23:50:45 +0800

    fixed bugs

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,9 @@
-for i in range(1, 100, -1):
-    if i % 3 == 0:
-        print("Fizz")
-    elif i % 5 == 0:
-        print("Buzz")
-    elif i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz")
-    else:
-        print(i)
+for num in range(1, 101):
+  if num % 3 == 0 and num % 5 == 0:
+    print('FizzBuzz')
+  elif num % 3 == 0:
+    print('Fizz')
+  elif num % 5 == 0:
+    print('Buzz')
+  else:
+    print(num)
