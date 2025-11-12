0ede2e1753897743138c783bdda22519bfb71058
mariellabanez <mariellabanez0814@gmail.com>
2025-11-12 11:39:30 +0800

    calculator in codedex

---
+++ calculator.py
@@ -0,0 +1,21 @@
+# Write code below 💖
+def add(x, y):
+  return x + y
+
+def subtract(a, b):
+  return a - b
+
+def multiply(a, b):
+  return a * b
+
+def divide(a, b):
+  return a / b
+
+def exp(a, b):
+  return a ** b
+
+print(add(3, 4))               # print (7)
+print(subtract(7, 5))          # print(2)
+print(multiply(5, 3))          # print (15)
+print(divide(40, 2))           # print(20)
+print(exp(3, 2))               # print(9)
