9c90d3adcb1b89ef13f0b69ca3ea47c51bc370bb
mariellabanez <mariellabanez0814@gmail.com>
2025-11-13 10:03:35 +0800

    slot_machine exercise

---
+++ BasicPythonExercises/modules_exercise/slot_machine.py
@@ -0,0 +1,11 @@
+import random
+
+symbols = ['🍒',' 🍇', '🍉', '7️⃣']
+
+results = random.choices(symbols, k=3)
+print(f'{results[0]} | {results[1]} | {results[2]}')
+
+if results == '7️⃣' and results == '7️⃣' and results == '7️⃣':
+    print("Jackpot! 💰")
+else:
+    print("Try Again.")
